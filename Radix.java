ee41e4d324f7b60ae42c5cc530f7f76d32cf2349
Grace <gchen20@stuy.edu>
2021-01-13 14:40:50 -0500

    Attempting to add new methods for radixSort and also fixed an error in nth

--- Radix.java
+++ Radix.java
@@ -8,7 +8,7 @@ public class Radix {
       modNum = modNum * 10;
     }
     n = n % modNum;
-    while (n > 10) {
+    while (n >= 10) {
       n = n / 10;
     }
     return n;
@@ -28,6 +28,16 @@ public class Radix {
 
   public static void merge(SortableLinkedList original, SortableLinkedList[]buckets) {
     for (int i = 0; i < buckets.length; i++) {
+      if (buckets[i] == null) {
+        SortableLinkedList placeHold = new SortableLinkedList();
+        buckets[i] = placeHold;
+      }
+      original.extend(buckets[i]);
+    }
+  }
+
+  public static void reverseMerge(SortableLinkedList original, SortableLinkedList[]buckets) {
+    for (int i = buckets.length-1; i >= 0; i--) {
       if (buckets[i] == null) {
         SortableLinkedList placeHold = new SortableLinkedList();
         buckets[i] = placeHold;
@@ -66,6 +76,58 @@ public class Radix {
       if (digit == 0) passes = length(max);
       merge(data, buckets);
     }
+  }
+
+  public static void negativeSort(SortableLinkedList data) {
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    int passes = 1;
+    for (int digit = 0; digit < passes; digit++) {
+      int max = 0;
+
+      while (data.size() > 0) {
+        int val = data.get(0);
+
+        if (digit == 0) {
+          if (val > max) {
+            max = val;
+          }
+        }
+
+        int numCol = nth(val, digit);
+        if (length(val) < digit+1) {
+          numCol = 0;
+        }
+        if (buckets[numCol] == null) {
+          SortableLinkedList placeHold = new SortableLinkedList();
+          buckets[numCol] = placeHold;
+        }
+        buckets[numCol].add(val);
+        data.remove(0);
+      }
+
+      if (digit == 0) passes = length(max);
+      reverseMerge(data, buckets);
+    }
+  }
+
+  public static SortableLinkedList radixSort(SortableLinkedList data) { //RETURN TO VOID
+    SortableLinkedList negData = new SortableLinkedList();
+    SortableLinkedList posData = new SortableLinkedList();
+    for (int i = data.size()-1; i >= 0; i--) {
+      if (data.get(i) < 0) {
+        negData.add(-(data.get(i)));
+      } else {
+        posData.add(data.get(i));
+      }
+      data.remove(i);
+    }
+    // radixSortSimple(posData);
+    // negativeSort(negData);
+    radixSortSimple(negData);
+    //
+    // data.extend(negData);
+    // data.extend(posData);
+    return negData;
   }
 
 }
--- Tester.java
+++ Tester.java
@@ -34,18 +34,25 @@ public class Tester {
     // System.out.println("After Merge: " + org);
 
     SortableLinkedList a = new SortableLinkedList();
-    a.add(12);a.add(34);a.add(42);a.add(32);a.add(44);
-    a.add(41);a.add(34);a.add(11);a.add(32);a.add(23);
-    a.add(87);a.add(50);a.add(77);a.add(58);a.add(8);
-    a.add(3); a.add(2);
-    System.out.println(a);
-    // System.out.println(Radix.radixSortSimple(a));
-    Radix.radixSortSimple(a);
-    System.out.println(a);
+    // a.add(12);a.add(34);a.add(42);a.add(32);a.add(44);
+    // a.add(41);a.add(34);a.add(11);a.add(32);a.add(23);
+    // a.add(87);a.add(50);a.add(77);a.add(58);a.add(8);
+    // a.add(3); a.add(2);
+    // System.out.println(a);
+    // // System.out.println(Radix.radixSortSimple(a));
+    // Radix.radixSortSimple(a);
+    // System.out.println(a);
 
     // SortableLinkedList[] buckets = new SortableLinkedList[10];
     // buckets[0] = a;
     // buckets[0].add(1);
     // System.out.println(buckets[0]);
+
+    a.add(-12); a.add(24); a.add(0); a.add(-100);
+    // System.out.println(Radix.radixSort(a));
+    System.out.println(Radix.nth(100, 2));
+    System.out.println(Radix.nth(400, 2));
+    // Radix.radixSort(a);
+    System.out.println(a);
   }
 }
